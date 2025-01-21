37a9cee3be225369f2b9a5d2603041c945c1fd57
vikas9872 <shanabhogvikas648.com>
2025-01-21 23:49:03 +0530

    initial commit

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -1,26 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Banner.css"
+import axios from '../axios';
+import requests from '../Requests/Requests';
 const Banner = () => {
+    const [movie, setMovie]=useState([]);
+    useEffect(()=>{
+        async function fetchData() {
+            const request=await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random()*request.data.results.length-1)
+                ]
+            )
+            return request;
+        }
+        fetchData();
+    },[])
+    console.log(movie)
     function truncate(string, n){
         return string?.length > n ? string.substr(0, n-1)+"..." : string
     }
     return (
         <header className="banner"
             style={{
-                backgroundImage: `url('./Images/netflixbanner.jpg')`,
+                backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
                 backgroundSize: "cover",
                 backgroundPosition: "center center"
             }}>
             <div className="banner_content">
                 <h1 className='banner_title'>
-                    Movie Name
+                    {movie?.title || movie?.name || movie?.original_name}
                 </h1>
                 <div className="banner_buttons">
                     <button className='banner_button'>Play</button>
                     <button className='banner_button'>My List</button>
                 </div>
                 <h1 className="banner_description">
-                    {truncate(`Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum autem, veritatis architecto dolorum libero rerum iusto aut doloribus ut accusamus.`,150)}
+                    {truncate(movie?.overview,150)}
                 </h1>
             </div>
             <div className="banner_fadeBottom" />
