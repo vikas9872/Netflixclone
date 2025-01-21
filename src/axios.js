3d28cf64db2de8ab38e50099f8286e698b096d09
vikas9872 <shanabhogvikas648.com>
2025-01-21 19:54:47 +0530

    first commit

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-// 93952a2e613136eae1ff95b53c688b50
+// 
 
 const instance=axios.create({
-    baseURL: "https://www.themoviedb.org/"
+    baseURL: "https://api.themoviedb.org/3/"
 })
