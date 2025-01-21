e8e6678f00fe49a953f6f436db5bf87b3251a6f8
vikas9872 <shanabhogvikas648.com>
2025-01-21 20:34:40 +0530

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
 const firebaseConfig = {
-    apiKey: PROCESS.env.FB_KEY,
-    authDomain: "netflixclone-29e4a.firebaseapp.com",
-    projectId: "netflixclone-29e4a",
-    storageBucket: "netflixclone-29e4a.firebasestorage.app",
-    messagingSenderId: "323083988408",
-    appId: "1:323083988408:web:bd10881cb0c86ba69bf34d"
-  };
+  apiKey: PROCESS.env.FB_KEY,
+  authDomain: "netflixclone-9da64.firebaseapp.com",
+  projectId: "netflixclone-9da64",
+  storageBucket: "netflixclone-9da64.firebasestorage.app",
+  messagingSenderId: "103920508704",
+  appId: "1:103920508704:web:d45614d28b4a171e5b8145",
+  measurementId: "G-P08WWB5S7F"
+};
