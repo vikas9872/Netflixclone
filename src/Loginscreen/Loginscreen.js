faa6e3057f00cb71fb80ff3fdd19ddd6a448da3b
vikas9872 <shanabhogvikas648.com>
2025-02-05 14:59:57 +0530

    loginscreen

--- src/Loginscreen/Loginscreen.js
+++ src/Loginscreen/Loginscreen.js
@@ -1,27 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Loginscreen.css'
+import Signupscreen from '../Signupscreen/Signupscreen'
 const Loginscreen = () => {
+  const[signIn,setSignIn]=useState(false);
   return (
     <div className="loginscreen">
       <div className="login_background">
         <img src="./Images/netflixlogo.png" alt="login_background" className='loginscreen_logo' />
-        <button className='loginscreen_button'>
+        <button className='loginscreen_button' onClick={()=>setSignIn(true)}>
           Sign In
         </button>
         <div className="loginscreen_gradient" />
       </div>
       <div className="loginscreen_body">
-        <>
-          <h1>Unlimited films, TV programmes and more.</h1>
-          <h2>Watch anywhere, Cancel at anytime</h2>
-          <h3>Ready to watch? Enter your email to create to restart your membership</h3>
-          <div className="loginscreen_input">
-            <form>
-              <input type="email" placeholder='Email Address'/>
-              <button className='loginscreen_getstarted'>GET STARTED</button>
-            </form>
-          </div>
-        </>
+        {
+          signIn
+            ? (<Signupscreen />)
+            : (
+              <>
+                <h1>Unlimited films, TV programmes and more.</h1>
+                <h2>Watch anywhere, Cancel at anytime</h2>
+                <h3>Ready to watch? Enter your email to create to restart your membership</h3>
+                <div className="loginscreen_input">
+                  <form>
+                    <input type="email" placeholder='Email Address' />
+                    <button className='loginscreen_getstarted' onClick={()=>setSignIn(true)}>GET STARTED</button>
+                  </form>
+                </div>
+              </>
+            )}
       </div>
     </div>
   )
---
+++ src/Signupscreen/Signupscreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Signupscreen.css"
+const Signupscreen = () => {
+  return (
+    <div className="signupscreen">
+        
+    </div>
+  )
+}
+
+export default Signupscreen
