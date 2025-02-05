3366bd455e59f35b685c63c30a1e5301af267bd9
vikas9872 <shanabhogvikas648.com>
2025-02-06 00:16:57 +0530

    auth in firebase

--- src/Signupscreen/Signupscreen.js
+++ src/Signupscreen/Signupscreen.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import "./Signupscreen.css"
 const Signupscreen = () => {
-  const register=(e)=>{
+  const register = (e) => {
     e.preventDefault();
   }
-  const signIn=(e)=>{
+  const signIn = (e) => {
     e.preventDefault();
-
+    
   }
   return (
     <div className="signupscreen">
-        <form>
-          <h1>Sign In</h1>
-          <input type="email" placeholder='Email' />
-          <input type="password" placeholder='Password'/>
-          <button type='submit' onClick={signIn}>Sign In</button>
-          <h4>
-            <span className='signupscreen_gray'>New to netflix?</span>
-            <span className='signupscreen_link' onClick={register}>Sign up now.</span>
-          </h4>
-        </form>
+      <form>
+        <h1>Sign In</h1>
+        <input type="email" placeholder='Email' />
+        <input type="password" placeholder='Password' />
+        <button type='submit' onClick={signIn}>Sign In</button>
+        <h4>
+          <span className='signupscreen_gray'>New to netflix?</span>
+          <span className='signupscreen_link' onClick={register}>Sign up now.</span>
+        </h4>
+      </form>
     </div>
   )
 }
