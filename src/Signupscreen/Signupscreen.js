6ce2ebc474319530f554abfcb49ae63908a3a8d5
vikas9872 <shanabhogvikas648.com>
2025-02-05 18:02:37 +0530

    signupage

--- src/Signupscreen/Signupscreen.js
+++ src/Signupscreen/Signupscreen.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import "./Signupscreen.css"
 const Signupscreen = () => {
+  const register=(e)=>{
+    e.preventDefault();
+  }
+  const signIn=(e)=>{
+    e.preventDefault();
+
+  }
   return (
     <div className="signupscreen">
         <form>
           <h1>Sign In</h1>
           <input type="email" placeholder='Email' />
           <input type="password" placeholder='Password'/>
-          <button type='submit'>Sign In</button>
+          <button type='submit' onClick={signIn}>Sign In</button>
+          <h4>
+            <span className='signupscreen_gray'>New to netflix?</span>
+            <span className='signupscreen_link' onClick={register}>Sign up now.</span>
+          </h4>
         </form>
     </div>
   )
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import firebase from "firebase"
 dotenv.config();
 const firebaseConfig = {
   apiKey: PROCESS.env.FB_KEY,
