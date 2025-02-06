dca68bdc4230f2bba5312db98c789cedf8d9e12c
vikas9872 <shanabhogvikas648.com>
2025-02-06 16:04:43 +0530

    loginandout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Homescreen from './Homescreen/Homescreen';
 import Loginscreen from './Loginscreen/Loginscreen';
 import { Routes, Route } from "react-router-dom";
+import { auth } from './firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectUser } from './features/counter/userSlice';
+import { login } from './features/counter/userSlice';
 function App() {
-  const user=null;
+  const user=useSelector(selectUser);
+  const dispatch=useDispatch()
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (userAuth) => {
+      if (userAuth) {
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email
+        }))
+      } else {
+        dispatch(logout)
+      }
+    });
+
+    return () => unsubscribe();
+  }, [dispatch]);
   return (
     <div className="App">
       <Routes>
